c6ad1e79c41d944327199e3f03b624094aa944ff
tupbich <AVildanov@tdera.ru>
2018-06-04 12:31:25 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,35 @@
-import { enableProdMode } from '@angular/core';
+import { NgModuleRef, ApplicationRef, enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { createNewHosts } from '@angularclass/hmr';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+const hmrBootstrap = (module: any, bootstrap: () => Promise<NgModuleRef<AppModule>>) => {
+  let ngModule: NgModuleRef<AppModule>;
+  module.hot.accept();
+
+  bootstrap().then(currentModule => {
+    ngModule = currentModule;
+  });
+
+  module.hot.dispose(() => {
+    const appRef: ApplicationRef = ngModule.injector.get(ApplicationRef);
+    const elements = appRef.components.map(c => c.location.nativeElement);
+    const removeOldHosts = createNewHosts(elements);
+    ngModule.destroy();
+    removeOldHosts();
+  });
+};
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
+
+if (module['hot']) {
+  hmrBootstrap(module, bootstrap);
+} else {
+  bootstrap();
+}
